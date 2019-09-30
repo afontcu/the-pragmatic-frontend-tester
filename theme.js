58022d81e692511329c2bf150526792b6eca7748
Adrià Fontcuberta <afontcu@gmail.com>
2019-09-30 19:55:01 +0200

    Changes from fsf

--- theme.js
+++ theme.js
@@ -5,31 +5,35 @@ export default syntaxHighlighterPrism({
   prism: {
     style,
   },
-  googleFont: 'https://fonts.googleapis.com/css?family=Lora:400,700|Patua+One',
+  googleFont:
+    'https://fonts.googleapis.com/css?family=Lora:400,700|Merriweather',
   font: 'Lora, Open Sans, sans-serif',
   heading: {
-    fontFamily: 'Patua One, Open Sans, sans-serif',
+    fontFamily: 'Merriweather, Open Sans, sans-serif',
     letterSpacing: '-0.05rem',
   },
   Slide: {
     padding: '0 6rem',
   },
   pre: {
-    fontSize: '4.5rem',
+    fontSize: '1.8rem',
     borderRadius: '12px',
     backgroundColor: 'inherit !important',
   },
   p: {
-    fontSize: '2.5em',
+    fontSize: '1.3em',
   },
   h1: {
-    fontSize: '3em',
+    textTransform: 'inherit',
+    fontSize: '2.1em',
   },
   h2: {
-    fontSize: '3.8em',
+    textTransform: 'inherit',
+    fontSize: '2.1em',
   },
   h3: {
-    fontSize: '1.5em',
+    textTransform: 'inherit',
+    fontSize: '1.7em',
   },
   code: {
     color: 'rgb(65,184,131)',
@@ -40,7 +44,8 @@ export default syntaxHighlighterPrism({
   },
   css: {
     strong: {
-      textShadow: '-2px -2px ##e7e7e7, -2px 2px ##e7e7e7, 2px -2px ##e7e7e7, 2px 2px ##e7e7e7',
+      textShadow:
+        '-2px -2px #e7e7e7, -2px 2px #e7e7e7, 2px -2px #e7e7e7, 2px 2px #e7e7e7',
       backgroundSize: '7px 1em',
       boxShadow: 'inset 0 -0.15em #e7e7e7, inset 0 -0.21em rgb(65,184,131)',
       display: 'inline',
