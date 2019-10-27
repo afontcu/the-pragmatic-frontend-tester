5faf558c2d001236fa2aa7a3d747bcbfd69a48b9
Adrià Fontcuberta <afontcu@gmail.com>
2019-10-27 19:56:22 +0100

    Redesigning... a bit

---
+++ Signature.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function() {
+  return (
+    <span
+      style={{
+        fontSize: '1.5rem',
+        position: 'absolute',
+        bottom: '2rem',
+        right: '2rem',
+      }}
+    >
+      @afontcu_
+    </span>
+  )
+}
--- theme.js
+++ theme.js
@@ -6,34 +6,40 @@ export default syntaxHighlighterPrism({
     style,
   },
   googleFont:
-    'https://fonts.googleapis.com/css?family=Lora:400,700|Merriweather',
-  font: 'Lora, Open Sans, sans-serif',
+    'https://fonts.googleapis.com/css?family=Merriweather:300,900|Raleway:900',
+  font: 'Merriweather, Open Sans, sans-serif',
   heading: {
-    fontFamily: 'Merriweather, Open Sans, sans-serif',
+    fontFamily: 'Raleway, Open Sans, sans-serif',
+    fontWeight: '900',
+    textTransform: 'uppercase',
     letterSpacing: '-0.05rem',
+    textAlign: 'left',
   },
   Slide: {
-    padding: '0 6rem',
+    padding: '0 20vw 0 10rem',
+    width: '100vw',
+    alignItems: 'flex-start',
   },
   pre: {
     fontSize: '1.8rem',
     borderRadius: '12px',
-    backgroundColor: 'inherit !important',
+    boxShadow: '0 10px 110px rgba(0,0, 0,0.3)',
+    padding: '4rem !important',
+    backgroundColor: '#fff !important',
   },
   p: {
+    textAlign: 'left',
     fontSize: '1.2em',
+    fontWeight: '300',
     margin: '1em 0',
   },
   h1: {
-    textTransform: 'inherit',
     fontSize: '2em',
   },
   h2: {
-    textTransform: 'inherit',
     fontSize: '1.7em',
   },
   h3: {
-    textTransform: 'inherit',
     fontSize: '1.2em',
   },
   code: {
@@ -45,11 +51,15 @@ export default syntaxHighlighterPrism({
   },
   css: {
     strong: {
+      fontWeight: '900',
       textShadow:
         '-2px -2px #e7e7e7, -2px 2px #e7e7e7, 2px -2px #e7e7e7, 2px 2px #e7e7e7',
       backgroundSize: '7px 1em',
       boxShadow: 'inset 0 -0.15em #e7e7e7, inset 0 -0.21em rgb(65,184,131)',
       display: 'inline',
+    },
+    small: {
+      textAlign: 'left',
     },
   },
   colors: {
